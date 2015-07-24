c1e796f7c5c2b33eb122ea305ab3e3af77d81dbf
Stephen Genusa <github@genusa.com>
2015-07-24 08:40:12 -0500

    My customconfig.py configuration

---
+++ mwlib/rl/customconfig.py
@@ -0,0 +1,16 @@
+######### PAGE CONFIGURATION
+#page_break_after_article = True # Turn Off/On page break after each article
+#show_article_attribution = False   # Show/Hide article source and contributors
+#pagefooter = 'This text is printed in the footer of all article pages' 
+#titlepagefooter = 'This text is only printed in the footer of the title page'
+#title_page_image = '/data/yourimagename.png' # Path of an image that is to be displayed on the title page
+
+creator="Stephen Genusa"
+
+######### TEXT CONFIGURATION
+import os
+os.environ['MATH_RESOLUTION'] = "120"
+
+######### IMAGE CONFIGURATION
+link_images = False
+image_warnings_to_ignore="Ambox warning yellow.svg","Cc.logo.circle.svg"
